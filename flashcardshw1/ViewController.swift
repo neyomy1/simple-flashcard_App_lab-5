10360e4e5f0fc63fd218989f5394586094442738
neyomy1 <104121392+neyomy1@users.noreply.github.com>
2022-09-21 22:25:22 -0400

    Initial Commit

---
+++ flashcardshw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flashcardshw1
+//
+//  Created by Naomy Chepngeno Chesengeny on 9/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
